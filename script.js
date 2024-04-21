d99556ecf924cfac44a5db1a516a5309824e72dc
Jordy4D_LinuxVM <jsadams27@gmail.com>
2024-04-21 12:09:15 -0400

    project init, skeleton files, book object outline

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead, isLiked) {
+    // the constructor...
+    // author, title, number of pages, whether it’s been read and anything else
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.isLiked = isLiked;
+
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
